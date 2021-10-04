d857a0205adb3e0d4f39fc3fdf6462421956b397
Masaki-isaka <ldistanceekdnm.issa09@gmail.com>
2021-10-05 00:51:41 +0900

    ajax通信

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     get "nogichecks/index"
     get "nogichecks/index", to: "nogichecks#index"
     get "nogichecks/question_1", to: "nogichecks#question_1"
+    post "nogichecks/question_1", to: "nogichecks#question_1"
     get "nogichecks/question_2", to: "nogichecks#question_2"
 end
