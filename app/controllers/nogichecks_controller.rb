b42fc2ef611fb21ee294e9fe3263258ac32c5745
Masaki-isaka <ldistanceekdnm.issa09@gmail.com>
2021-10-27 21:23:48 +0900

    回答反映

--- app/controllers/nogichecks_controller.rb
+++ app/controllers/nogichecks_controller.rb
@@ -13,7 +13,7 @@ class NogichecksController < ApplicationController
     end
     return redirect_to action: :index if @sort < 1
     if @sort == 1
-      QuestionSort.where(user_id: user_id).delete_all
+      QuestionSort.where(user_id: user_id).destroy_all
       question_ids = Question.order("RANDOM()").limit(2).pluck(:id)
       question_ids.each.with_index(1) do |question_id, index|
         QuestionSort.create!(user_id: user_id, question_id: question_id, sort: index)
@@ -27,6 +27,7 @@ class NogichecksController < ApplicationController
       @maniac = "MAX"
       @result = "正真正銘のオタクです"
       @answer = Option.joins(question: :question_sorts).where(question_sorts: {user_id: user_id, sort: [1,2]}, is_answer: true)
+      @your_answer = Judge.joins(:question_sort).where(question_sorts: {user_id: user_id}).pluck(:is_answer)
       render :result  
     end
   end
